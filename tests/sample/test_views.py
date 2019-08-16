a54339c8b2f2489ff11b0d77b4d680537a64e9c8
西村唯 <ynishimura@nishimurayuinoMacBook-Air.local>
2019-08-16 19:07:57 +0900

    テスト追加

--- tests/sample/test_views.py
+++ tests/sample/test_views.py
@@ -3,13 +3,13 @@ from django.urls import reverse
 
 class SampleViewTest(TestCase):
     def test_get_index_01(self):
-        print('acccess_url={}'.format(reverse('sample:index')))
+        # print('acccess_url={}'.format(reverse('sample:index')))
         response = self.client.get(reverse('sample:index'))
         self.assertTemplateUsed(response, 'sample/index.html')
 
     def test_post_index_01(self):
         response = self.client.post(reverse('sample:index'), data={})
-        self.assertTemplateNotUsed(response, 'sample/index.html')
+        self.assertTemplateUsed(response, 'sample/index.html')
 
     def test_post_index_02(self):
         response = self.client.post(reverse('sample:index'), data={'message': 'Test Message'})
