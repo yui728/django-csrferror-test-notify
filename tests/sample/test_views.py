de09195153c86b1921fc183553fbace3b88c7cff
西村唯 <ynishimura@nishimurayuinoMacBook-Air.local>
2019-08-20 16:41:18 +0900

    TestCaseでCSRFトークンのチェックを行うように修正

--- tests/sample/test_views.py
+++ tests/sample/test_views.py
@@ -1,16 +1,27 @@
 from django.test.testcases import TestCase
 from django.urls import reverse
+from django.test.client import Client
 
 class SampleViewTest(TestCase):
+
+    def _pre_setup(self):
+        super()._pre_setup()
+        self.client = Client(enforce_csrf_checks=True)
+
     def test_get_index_01(self):
-        # print('acccess_url={}'.format(reverse('sample:index')))
         response = self.client.get(reverse('sample:index'))
         self.assertTemplateUsed(response, 'sample/index.html')
 
     def test_post_index_01(self):
         response = self.client.post(reverse('sample:index'), data={})
-        self.assertTemplateUsed(response, 'sample/index.html')
+        # If csrf_token was template given.
+        # self.assertTemplateUsed(response, 'sample/index.html')
+        # If csrf_token was't template given.
+        self.assertEquals(403, response.status_code)
 
     def test_post_index_02(self):
         response = self.client.post(reverse('sample:index'), data={'message': 'Test Message'})
-        self.assertRedirects(response, reverse('sample:index'))
+        # If csrf_token was template given.
+        # self.assertRedirects(response, reverse('sample:index'))
+        # If csrf_token was't template given.
+        self.assertEquals(403, response.status_code)
